5bf685ecb5b45ea9b5aa7233ffce17a3396c81fc
cubasve <62129720+cubasve@users.noreply.github.com>
2020-06-09 01:20:31 -0400

    Remove comments

--- js/js/main.js
+++ js/js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
-    var $addSkill = $("#addSkill"); //button
-    var $inputSkill = $("#enterSkill"); //input box
-    var $skillList = $('#listOfSkill'); //table list of skills
-    var $removeSkill = $('.deleteSkill'); //newly created button beside each added skill
+    var $addSkill = $("#addSkill"); 
+    var $inputSkill = $("#enterSkill"); 
+    var $skillList = $('#listOfSkill'); 
+    var $removeSkill = $('.deleteSkill'); 
 
     $addSkill.on('click', function() {
-        if ($inputSkill.val() !== " ") { //if value of inputBox is not equal to an empty string --> meaning there is content in it
+        if ($inputSkill.val() !== " ") { 
             var inputValue = $inputSkill.val();
             var newSkill = $(`<tr>
                                 <td><button class="deleteSkill"> X </button></td> 
@@ -17,17 +17,7 @@ $(document).ready(function() {
         }
     });
 
-    $skillList.on('click', 'button', function() { //need 3 parameters - add'button'
+    $skillList.on('click', 'button', function() { 
         $(this).closest('tr').remove();
     });
 });
-
-
-// const btnEl = document.querySelector('button');
-// btn.addEventListener('click', function(evt) {
-//    const li = document.createElement('li');             //1
-//    const inp = document.querySelector('input');     
-//    li.textContent = inp.value;                                  //2
-//    document.querySelector('ul').appendChild('li');  //3
-//    inputEl.value = ' ';     //4
-// });
